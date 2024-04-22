9be3c2dafdc3b256c09b775fc766d82ec6a07cb9
fernando-mac <luizfsegato@gmail.com>
2024-04-22 18:55:14 -0400

    DockerFile

---
+++ src/main/java/com/exercise/springboot/configs/security/WebConfig.java
@@ -0,0 +1,10 @@
+package com.exercise.springboot.configs.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+}
